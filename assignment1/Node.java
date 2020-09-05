9e9613d88b05545ca14ec3916e9530efb6501a11
Joseph McDonough <joseph.mcdonough1@marist.edu>
2020-09-05 11:27:51 -0400

    Created class file for Nodes, Stacks, and Queues.  Add code for nodes

---
+++ assignment1/Node.java
@@ -0,0 +1,38 @@
+/*
+Joseph McDonough
+CMPT 435
+Professor Labouseur
+18 September 2020
+ */
+
+public class Node {
+    String data;     //contents of node are strings
+    Node reference;  //tail of node points to next node in linked list
+
+    //Creates node that holds no data
+    public Node()
+    {
+        data = "";
+        reference = null;
+    }
+
+    //Creates node with data
+    public Node(String s) {
+        data = s;
+        reference = null;
+    }
+
+    //Sets pointer of node to the next node in the linked list
+    public void nextNode(Node n)
+    {
+        this.reference = n;
+    }
+}
+
+class Stack {
+
+}
+
+class Queue {
+
+}
