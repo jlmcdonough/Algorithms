55a080129d33a0d3d8bd1fc2482feb79daec11a3
Joseph McDonough <joseph.mcdonough1@marist.edu>
2020-09-12 11:14:24 -0400

    Added file reader

--- assignment1/Assignment1.java
+++ assignment1/Assignment1.java
@@ -5,39 +5,38 @@ Professor Labouseur
 18 September 2020
  */
 
-public class Assignment1 {
-    public static void main(String[] args) {
-        System.out.println(1 + 1);
-        Stack s = new Stack();
-        Node n1 = new Node("alpha");
+import java.io.*;
+import java.net.URL;
+import java.util.*;
 
-        Node n2 = new Node("bravo");
+public class Assignment1
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            URL path = Assignment1.class.getResource("magicitems.txt");
+            File magicFile = new File(path.getFile());
+            System.out.println(magicFile);
+            Scanner reader = new Scanner(magicFile);
+            ArrayList<String> myMagicItems = new ArrayList<String>();
+            ArrayList<String> myPalindromes = new ArrayList<String>();
+            while (reader.hasNextLine())
+            {
+                String item = reader.nextLine();
+                item = item.toLowerCase().trim().replaceAll(" ","");
+                item = item.replaceAll("\\p{Punct}", "");
+                myMagicItems.add(item);
+            }
 
-        Node n3 = new Node("charlie");
 
-        n1.nextNode(n2);
-        n2.nextNode(n3);
+        }
 
-        s.push("n1");
-        System.out.println(s.pop().data);
-        s.push("n2");
-        System.out.println(s.pop().data);
-        s.push("n3");
-        System.out.println(s.pop());System.out.println(s.pop());System.out.println(s.pop());
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found");
+        }
 
-
-        Queue q = new Queue();
-        System.out.println(q.dequeue());
-        q.enqueue("alpha");
-        System.out.println(q.dequeue().data);
-        q.enqueue("bravo");
-        q.enqueue("charlie");
-        q.enqueue("delta");
-        System.out.println(q.queueFirst.data + ' ' + q.queueLast.data);
-        System.out.println(q.dequeue().data);
-        System.out.println(q.dequeue().data);
-        q.enqueue("echo");
-        System.out.println(q.dequeue().data);
     }
 }
 
