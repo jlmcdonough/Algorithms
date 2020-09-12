63bdf304186b2f68046ba35151152a34d9a077da
Joseph McDonough <joseph.mcdonough1@marist.edu>
2020-09-12 11:31:59 -0400

    Read through file and add palidromes to new list

--- assignment1/Assignment1.java
+++ assignment1/Assignment1.java
@@ -29,7 +29,40 @@ public class Assignment1
                 myMagicItems.add(item);
             }
 
+            for(int i = 0; i < myMagicItems.size(); i++)
+            {
+                Stack s = new Stack();
+                Queue q1 = new Queue();
+                for(int j = 0; j < myMagicItems.get(i).length(); j++)
+                {
+                    String letter = Character.toString(myMagicItems.get(i).charAt(j));
+                    s.push(letter);
+                    q1.enqueue(letter);
+                }
 
+                boolean isPalindrome = true;
+                int k = 0;
+                while(k < myMagicItems.get(i).length())
+                {
+                    String stackPop = s.pop().data;
+                    String queueDequeue = q1.dequeue().data;
+                    if(isPalindrome && (stackPop.equals(queueDequeue)))
+                        k++;
+                    else if(isPalindrome && !((stackPop.equals(queueDequeue))))
+                    {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+
+                if(s.isEmpty() && isPalindrome)
+                    myPalindromes.add(myMagicItems.get(i));
+
+            }
+
+            System.out.println(myPalindromes.size());
+            for(String palindromes : myPalindromes )
+                System.out.println(palindromes);
         }
 
         catch (FileNotFoundException e)
