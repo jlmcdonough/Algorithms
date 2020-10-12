ea37607642aa4a2fc6136d8af7675f06b2602bbe
Joseph McDonough <joseph.mcdonough1@marist.edu>
2020-10-12 10:57:44 -0400

    Basic function outline for project

---
+++ semester-project/InfectionSimulation.java
@@ -0,0 +1,42 @@
+/*
+Joseph McDonough
+CMPT 435
+Professor Labouseur
+11 December 2020
+ */
+
+import java.util.*;
+
+public class InfectionSimulation
+{
+    public static void main(String args[])
+    {
+
+    }
+
+    public static void createPopulation(TAKE USER INPUT FOR GROUP STUFF)
+    {
+        //infectPopulation()
+    }
+
+    public static void infectPopulation(TAKE INFO FROM ABOVE)
+    {
+
+    }
+
+    public static void divideIntoGroups(TAKE FROM createPopulation)
+    {
+        //if population is not divisible by group size, round up or down so that it is
+    }
+
+    public static String showResults()
+    {
+        /*
+        Case (1): 125 × 0.8500 = 106.25 instances requiring 107 tests (there are no partial tests)
+        Case (2): 125 × 0.1496 = 18.70 instances requiring 131 tests
+        Case (3): 125 × 0.0004 = 0.05 round up to 1 instance requiring 11 tests
+        ———————————————————————————————————————
+        249 tests to screen a population of 1000 people for a disease with 2% infection rate.
+         */
+    }
+}
