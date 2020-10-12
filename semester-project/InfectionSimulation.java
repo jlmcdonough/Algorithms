6911fe188ae31bd37fe16407ef65540f884f194b
Joseph McDonough <joseph.mcdonough1@marist.edu>
2020-10-12 12:05:46 -0400

    Added and kinda formatted basic user input for population size, infection rate, group size, and test accuracy with some error checking

--- semester-project/InfectionSimulation.java
+++ semester-project/InfectionSimulation.java
@@ -11,9 +11,71 @@ public class InfectionSimulation
 {
     public static void main(String args[])
     {
+        inputLoop();
+        verifyInput();
 
     }
 
+    public static void getUserInput()
+    {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter the population size: ");
+        int popSize = myScanner.nextInt();
+        System.out.print("Enter infection rate (as percentage - e.g. 90% is 90): ");
+        double infectionRate = myScanner.nextDouble();
+        System.out.print("Enter testing group size: ");
+        int groupSize = myScanner.nextInt();
+        System.out.print("Enter test accuracy rate (as percentage - e.g. 90% is 90): ");
+        double testAccuracy = myScanner.nextInt();
+
+        System.out.println("Population Size: " + popSize +
+                "\nInfection Rate: " + infectionRate + "%" +
+                "\nGroup Size: " + groupSize +
+                "\nTest Accuracy: " + testAccuracy + "%");
+
+        //myScanner.close();
+    }
+
+    public static void inputLoop()
+    {
+        boolean haveData = false;
+        while(!haveData)
+        {
+            try
+            {
+                getUserInput();
+                haveData = true;
+            }
+            catch (Exception InputMismatchException)
+            {
+                System.out.println("Please only enter numbers");
+            }
+        }
+    }
+
+    public static void verifyInput()
+    {
+        Scanner myScanner = new Scanner(System.in);
+        boolean verifiedInput = false;
+        while (!verifiedInput)
+        {
+            System.out.print("Are these values correct (Y/N): ");
+            String yesOrNo = myScanner.nextLine();
+            if (yesOrNo.equalsIgnoreCase("N"))
+                inputLoop();
+            else if (yesOrNo.equalsIgnoreCase("Y"))
+            {
+                System.out.println("Values Confirmed");
+                verifiedInput = true;
+            } else
+              {
+                System.out.println("Please only enter 'Y' or 'N' ");
+                verifyInput();
+              }
+        }
+    }
+
+/*
     public static void createPopulation(TAKE USER INPUT FOR GROUP STUFF)
     {
         //infectPopulation()
@@ -37,6 +99,6 @@ public class InfectionSimulation
         Case (3): 125 × 0.0004 = 0.05 round up to 1 instance requiring 11 tests
         ———————————————————————————————————————
         249 tests to screen a population of 1000 people for a disease with 2% infection rate.
-         */
-    }
+
+    } */
 }
