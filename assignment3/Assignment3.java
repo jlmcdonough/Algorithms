ec53b249ba256247b8a166fd2ec993a7e50274f9
Joseph McDonough <joseph.mcdonough1@marist.edu>
2020-10-16 17:29:22 -0400

    Moved all of linear sort stuff into its own function. Resolves #18.  Also created a function to do the random stuff and return the randomItem item list.  Also moved the get file, read file, and sort to a function that returns a sorted list

--- assignment3/Assignment3.java
+++ assignment3/Assignment3.java
@@ -11,52 +11,15 @@ import java.io.*;
 
 public class Assignment3
 {
-    public static void main(String args[])
+    public static void main(String args[]) throws FileNotFoundException
     {
-        try
-        {
-            Scanner reader = new Scanner(new File("magicitems.txt"));
-            ArrayList<String> myMagicItems = new ArrayList<String>();  //will be the sorted list
-            ArrayList<String> myMagicLinear = new ArrayList<>();      //mutated by random items
+            ArrayList<String> myMagicItems = getAndSortList();
 
-            while (reader.hasNextLine())
-                myMagicItems.add(reader.nextLine());
+            ArrayList<String> myMagicRandom = new ArrayList<>();      //mutated by random items
+            myMagicRandom = (ArrayList)myMagicItems.clone();
+            String[] randomItems = generateRandomItems(myMagicRandom);
 
-            mergeSort(myMagicItems, myMagicItems.size());
-            myMagicLinear = (ArrayList)myMagicItems.clone();  //create a cl
-
-            String[] randomItems = new String[42];
-            Random rand = new Random();
-            for(int i = 0; i < randomItems.length; i++)  //assignment wants 42 random items
-            {
-                int randIndex = rand.nextInt(myMagicLinear.size());  //choosing myMagicItems so can remove items without messing up search
-                randomItems[i] = myMagicLinear.get(randIndex);
-                myMagicLinear.remove(randIndex);  //dont want to get duplicates so removing once its gotten
-            }
-
-            ArrayList<Assignment3> linearSortedGroup = new ArrayList<Assignment3>();
-            for(int j = 0; j < randomItems.length; j++)
-            {
-                linearSortedGroup = linearSort(myMagicItems, randomItems[j], linearSortedGroup);
-            }
-
-            //longest item is 46 characters long, so ensured 1 padding on each side
-            //largest number is 3 characters long, so ensured 1 padding on each side
-            System.out.println("LINEAR SEARCH\n");
-            System.out.format("%-48s%-5s%n", "ITEM", "COUNT");
-            int linearTotalCount = 0;
-            for(int k = 0; k < linearSortedGroup.size(); k++)
-            {
-                linearTotalCount += linearSortedGroup.get(k).getCount();
-                System.out.format("%-48s%-5d%n", linearSortedGroup.get(k).getItem(), linearSortedGroup.get(k).getCount());
-            }
-            System.out.println("AVERAGE SEARCH COUNT: " + (linearTotalCount/42));
-        }
-
-        catch (FileNotFoundException e)
-        {
-            System.out.println("File not found");
-        }
+            linearSortComplete(myMagicItems, randomItems);
     }
 
     //object that holds search item and counts taken to find it as a pair
@@ -79,6 +42,32 @@ public class Assignment3
         return this.searchCount;
     }
 
+    public static ArrayList<String> getAndSortList() throws FileNotFoundException
+    {
+        Scanner reader = new Scanner(new File("magicitems.txt"));
+        ArrayList<String> myMagicItems = new ArrayList<String>();  //will be the sorted list
+
+        while (reader.hasNextLine())
+            myMagicItems.add(reader.nextLine());
+
+        mergeSort(myMagicItems, myMagicItems.size());
+
+        return myMagicItems;
+    }
+
+    public static String[] generateRandomItems(List<String> inputList)
+    {
+        String[] randomItems = new String[42];
+        Random rand = new Random();
+        for(int i = 0; i < randomItems.length; i++)  //assignment wants 42 random items
+        {
+            int randIndex = rand.nextInt(inputList.size());  //choosing myMagicItems so can remove items without messing up search
+            randomItems[i] = inputList.get(randIndex);
+            inputList.remove(randIndex);  //dont want to get duplicates so removing once its gotten
+        }
+
+        return randomItems;
+    }
 
     public static ArrayList<Assignment3> linearSort(List<String> myList, String item, ArrayList<Assignment3> linearCountArray)
     {
@@ -100,6 +89,32 @@ public class Assignment3
         return linearCountArray;
     }
 
+    public static void linearSortComplete(List<String> magicItems, String[] randomItems)
+    {
+        ArrayList<Assignment3> linearSortedGroup = new ArrayList<Assignment3>();
+        for(int j = 0; j < randomItems.length; j++)
+        {
+            linearSortedGroup = linearSort(magicItems, randomItems[j], linearSortedGroup);
+        }
+
+        System.out.println("LINEAR SEARCH\n");
+        printSearchResults(linearSortedGroup);
+    }
+
+
+    public static void printSearchResults(List<Assignment3> sortList)
+    {
+        //longest item is 46 characters long, so ensured 1 padding on each side
+        //largest number is 3 characters long, so ensured 1 padding on each side
+        System.out.format("%-48s%-5s%n", "ITEM", "COUNT");
+        int totalCount = 0;
+        for(int k = 0; k < sortList.size(); k++)
+        {
+            totalCount += sortList.get(k).getCount();
+            System.out.format("%-48s%-5d%n", sortList.get(k).getItem(), sortList.get(k).getCount());
+        }
+        System.out.println("AVERAGE SEARCH COUNT: " + (totalCount/42));
+    }
 
 
 
