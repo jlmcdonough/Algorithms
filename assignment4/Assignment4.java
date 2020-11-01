0547ced6e006ee28757b06ca35f1038698b1a60a
Joseph McDonough <joseph.mcdonough1@marist.edu>
2020-11-01 16:55:35 -0500

    Created a graph subclass as well as got the graphs to be read and stored in their own ArrayLists.  Adding graphs1.txt file to the repository as well. Resolves #29.

--- assignment4/Assignment4.java
+++ assignment4/Assignment4.java
@@ -5,6 +5,7 @@ Professor Labouseur
 13 November 2020
  */
 
+import java.awt.image.AreaAveragingScaleFilter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
@@ -17,7 +18,110 @@ public class Assignment4 extends Tree
         String[] myRandomItems = generateRandomItems(myMagicItems);
 
         binaryTreeComplete(myMagicItems, myRandomItems);
+
+
+
+        ArrayList<String> graphFile = readGraphFile();
+
+        System.out.println(graphFile.get(0).substring(0,2));
+        System.out.println(graphFile.get(10).substring(0,8));
+        ArrayList<Graph> myGraphs = new ArrayList<Graph>();
+
+        Boolean isVertex = (graphFile.get(4).substring(0,10).equalsIgnoreCase("add vertex"));
+
+        Boolean isEdge = (graphFile.get(11).substring(0,8).equalsIgnoreCase("add edge"));
+
+        if(isVertex)
+        {
+            String vertex = graphFile.get(4).substring(11);
+            System.out.println("V:" + vertex);
+        }
+
+        if(isEdge)
+        {
+            String edgeCombo = graphFile.get(11).substring(9);
+            String[] edges = edgeCombo.split(" - ");
+            int edge1 = Integer.parseInt(edges[0]);
+            int edge2 = Integer.parseInt(edges[1]);;
+            System.out.println("E1:" + edge1 + "E2:" + edge2);
+        }
+
+        int i = 0;
+        boolean inGraph = false;
+        while(i < graphFile.size())
+        {
+
+            if(graphFile.get(i).substring(0,2).equalsIgnoreCase("--"))
+            {
+                inGraph = true;
+                ArrayList<ArrayList<Integer>> vertex = new ArrayList<ArrayList<Integer>>();
+                if(!graphFile.get(i).contains("Zork"))
+                    vertex.add(null);   //vertices all start at value 1, so going to make index 0 null such that they can start at one, except in Zork graph
+                ArrayList<Integer> edge = new ArrayList<Integer>();
+                String graphName = graphFile.get(i).substring(3);
+
+                i++;
+
+                while(inGraph && i <= graphFile.size())
+                {
+                    if(i == graphFile.size() || graphFile.get(i).equalsIgnoreCase(""))
+                    {
+                        inGraph = false;
+                        myGraphs.add(new Graph(graphName ,vertex));
+                    }
+                    else if(graphFile.get(i).equalsIgnoreCase("new graph"))
+                        System.out.println("Creating new graph");
+                    else if(graphFile.get(i).substring(0,10).equalsIgnoreCase("add vertex"))
+                    {
+                        vertex.add(new ArrayList<Integer>());
+                    }
+                    else if(graphFile.get(i).substring(0,8).equalsIgnoreCase("add edge"))
+                    {
+                        String edgeCombo = graphFile.get(i).substring(9);
+                        String[] edges = edgeCombo.split(" - ");
+                        int edge1 = Integer.parseInt(edges[0]);
+                        int edge2 = Integer.parseInt(edges[1]);
+
+                        System.out.println("E1:" + edge1 + "E2:" + edge2);
+
+                        ArrayList<Integer> temp1 = vertex.get(edge1);
+                        ArrayList<Integer> temp2 = vertex.get(edge2);
+
+                        System.out.println(temp1);
+                        System.out.println(temp2);
+
+                        temp1.add(edge2);
+                        temp2.add(edge1);
+                    }
+
+                    i++;
+
+                }
+            }
+
+        }
+
+        for(Graph f : myGraphs)
+        {
+            System.out.println(f.name);
+            System.out.println(f.vertex);
+        }
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
 
     //~~~~~~~~~~~~~~~~~ASSIGNMENT VARIABLES AND RESULT CONSTRUCTOR~~~~~~~~~
     private int searchCount;
@@ -37,6 +141,7 @@ public class Assignment4 extends Tree
     {
         return this.searchCount;
     }
+
 
     //~~~~~~~~~~~~~~~~~~READ FILE AND GET RANDOM ITEMS~~~~~~~~~~~~~~~~~~~~~~
     //does the work of the previous assignment of reading the file
@@ -114,4 +219,31 @@ public class Assignment4 extends Tree
         System.out.println("AVERAGE SEARCH COUNT: " + (totalCount/42));
     }
 
+
+    //~~~~~~~~~~~~GRAPH STUFF~~~~~~~~~~~~~~~~~~
+
+    public static ArrayList<String> readGraphFile() throws FileNotFoundException
+    {
+        Scanner in = new Scanner(new File("graphs1.txt"));
+        ArrayList<String> graph1Data = new ArrayList<String>();
+        while(in.hasNextLine())
+            graph1Data.add(in.nextLine());
+
+        return graph1Data;
+    }
+
+    static class Graph
+    {
+        private ArrayList<ArrayList<Integer>> vertex;
+        private ArrayList<Integer> edge;
+        private String name;
+
+        public Graph(String n, ArrayList<ArrayList<Integer>> v)
+        {
+            this.name = n;
+            this.vertex = v;
+        }
+        
+
+    }
 }
